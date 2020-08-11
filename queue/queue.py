d19c3dc20cbdeef4291dca30d76c8b9c5508ca3d
Kate McGee <mcgeehee@gmail.com>
2020-08-10 23:18:15 -0400

    queue working properly

--- queue/queue.py
+++ queue/queue.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('../singly_linked_list/')
+from singly_linked_list import LinkedList
+from singly_linked_list import Node 
+
 """
 A queue is a data structure whose primary purpose is to store and
 return elements in First In First Out order. 
@@ -16,13 +21,47 @@ Stretch: What if you could only use instances of your Stack class to implement t
 class Queue:
     def __init__(self):
         self.size = 0
-        # self.storage = ?
+        self.storage = LinkedList()
     
-    def __len__(self):
-        pass
+    def len (self):
+        if(self.storage.head == None and self.storage.tail == None):
+            return self.size
+        if(self.storage.head == self.storage.tail):
+            self.size = 1
+            return self.size
+        current = self.storage.head 
+        # keep iterating until the node after `current` is the tail
+        while current.get_next() != self.storage.tail:
+             # keep iterating 
+            current = current.get_next()
+            self.size += 1
+        self.size += 2 # one for the head and one for the tail
+        return self.size
+
+        while self.storage.get_next() != self.storage.tail:
+            # keep iterating 
+            self.size += 1
+        self.size += 1    
+        return self.size
 
     def enqueue(self, value):
-        pass
+        # * `enqueue` adds an element to the back of the queue.
+        new_node = Node(value)
+        self.storage.add_to_tail(value)
+        return self.storage
 
     def dequeue(self):
-        pass
+        #* `dequeue` removes and returns the element at the front of the queue.
+        return self.storage.remove_head()
+
+
+    
+newQ = Queue()
+newQ.enqueue(1)
+newQ.enqueue(2)
+
+print(newQ.len())
+print(newQ.dequeue())
+
+    
+  
